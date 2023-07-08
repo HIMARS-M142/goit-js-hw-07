40ac466e429f178769d7e1b47e48ea922f357e9f
HIMARS-M142 <maksympona@gmail.com>
2023-07-08 15:54:57 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const ulListEl = document.querySelector(".gallery");
+
+galleryItems.map((gall) => {
+  const createMarkup = `<li class="gallery__item">
+  <a class="gallery__link" href="${gall.original}">
+    <img
+      class="gallery__image"
+      src="${gall.preview}"
+      data-source="${gall.original}"
+      alt="${gall.description}"
+    />
+  </a>
+</li>`;
+  ulListEl.insertAdjacentHTML("afterbegin", createMarkup);
+});
+ulListEl.addEventListener("click", onGalleryClick);
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  ulListEl.addEventListener("keydown", onGalleryKey);
+  function onGalleryKey(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      ulListEl.removeEventListener("keydown", onGalleryKey);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryCreateEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryItems.map((gall) => {
+  const createMarkup = `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${gall.original}">
+      <img class="gallery__image" src="${gall.preview}" alt="${gall.description}" />
+   </a>
+</li>`;
+
+  galleryCreateEl.insertAdjacentHTML("afterbegin", createMarkup);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
